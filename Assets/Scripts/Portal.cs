68e27c3590140bbadc74d0aeeed6c38b6f090bf5
Jeremy Howa <jrhowa@inmotionsoftware.com>
2018-01-22 16:20:21 -0600

    Added Project Code

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+//https://www.youtube.com/playlist?list=PLKIKuXdn4ZMhwJmPnYI0e7Ixv94ZFPvEP
+
+public class Portal : MonoBehaviour
+{
+    public Transform device;
+    bool wasInFront;
+    bool inOtherWorld;
+    bool isColliding;
+
+    void Start()
+    {
+        SetMaterials(false);
+    }
+
+    void SetMaterials(bool fullRender)
+    {
+        var stencilTest = fullRender ? CompareFunction.NotEqual : CompareFunction.Equal;
+        Shader.SetGlobalInt("_StencilTest", (int)stencilTest);
+    }
+
+    bool GetIsInFront()
+    {
+        Vector3 worldPos = device.position + device.forward * Camera.main.nearClipPlane;
+        Vector3 pos = transform.InverseTransformPoint(worldPos);
+
+        return pos.z >= 0 ? true : false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform != device)
+            return;
+
+        wasInFront = GetIsInFront();
+        isColliding = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform != device)
+            return;
+
+        isColliding = false;
+    }
+
+    void WhileCameraColliding()
+    {
+        if (!isColliding)
+            return;
+
+        bool isInFront = GetIsInFront();
+
+        if ((isInFront && !wasInFront) || (wasInFront && !isInFront))
+        {
+            inOtherWorld = !inOtherWorld;
+            SetMaterials(inOtherWorld);
+        }
+
+        wasInFront = isInFront;
+    }
+
+    void OnDestroy()
+    {
+        SetMaterials(true);
+    }
+
+    void Update()
+    {
+        WhileCameraColliding();
+    }
+}
---
+++ Assets/Shaders/PortalWindow.shader
@@ -0,0 +1,23 @@
+﻿Shader "Custom/PortalWindow"
+{
+	SubShader
+	{	
+		//render objects behind the portal
+		ZWrite off
+		//absolutely transparent
+		ColorMask 0
+		//Bidirectional behaviour
+		Cull off
+
+		Stencil
+		{
+			Ref 1
+			//set all pixels in the portal to 1
+			Pass replace
+		}
+
+		Pass
+		{
+		}
+	}
+}
